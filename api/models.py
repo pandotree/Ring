cc1983179bd85aa1153bcc2325c23f2298388a31
Joseph Zhong <pandotree@gmail.com>
2012-07-26 09:00:22 -0700

    Added basic messages model; changed DateFields to DateTimeFields

--- api/models.py
+++ api/models.py
@@ -6,12 +6,17 @@ from django.contrib.auth.models import User
 class Users(models.Model):
 	user = models.OneToOneField(User)
 	university = models.CharField(max_length=32)
+  phone_number = us.models.PhoneNumberField()
 
 class Groups(models.Model):
 	group_name = models.CharField(max_length=64)
 	group_id = models.AutoField(primary_key=True)
-	created = models.DateField()
+	created = models.DateTimeField()
 	user_id = models.ManyToManyField(User, related_name="user_set")
 
+class Messages(models.Model):
+  sent = models.DateTimeField()
+  subject = models.TextField() #Not sure if we should limit subject line with CharField
+  content = models.TextField()
 
 #TODO VALIDATION
