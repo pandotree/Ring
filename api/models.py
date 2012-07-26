5cf074e6aeef47c71c47b195f18a97f0175a7df2
Joseph Zhong <pandotree@gmail.com>
2012-07-26 09:00:22 -0700

    settings.py commited so syncdb will work, phone_number field added to users model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.localflavor.us.forms import USPhoneNumberField
 from django.contrib.auth.models import User
 
 #Create models here
@@ -6,12 +7,18 @@ from django.contrib.auth.models import User
 class Users(models.Model):
 	user = models.OneToOneField(User)
 	university = models.CharField(max_length=32)
+	# phone_number = USPhoneNuberField() #
+	phone_number = models.IntegerField()
 
 class Groups(models.Model):
 	group_name = models.CharField(max_length=64)
 	group_id = models.AutoField(primary_key=True)
-	created = models.DateField()
+	created = models.DateTimeField()
 	user_id = models.ManyToManyField(User, related_name="user_set")
 
+class Messages(models.Model):
+	sent = models.DateTimeField()
+	subject = models.TextField() #Not sure if we should limit subject line with CharField
+	content = models.TextField()
 
 #TODO VALIDATION
