adbad051d020908ecd592a76d5c409c7741d1e0f
Grace Wang <gracewang92@gmail.com>
2012-07-29 23:33:27 -0700

    not working. csrf is unhappy

---
+++ Ring/static/js/user.js
@@ -0,0 +1,18 @@
+user = {
+    // default: first create a user
+
+    createUser: function(query){
+        var ok = false;
+
+        $.ajax('/create_user/',{
+            type: 'post',
+            data: query,
+            dataType: 'json',
+            success: function(data){
+                ok = true;
+            },
+            error: handleAjaxError,
+        });
+        return ok;
+    },
+};
