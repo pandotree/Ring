8aee406450cc4f1688f23d4fe116e086dad3b661
Joseph Zhong <pandotree@gmail.com>
2012-07-31 01:14:43 -0700

    undid shitty fixes for csrf

--- Ring/settings.py
+++ Ring/settings.py
@@ -97,12 +97,16 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     #'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+CONTEXT_PROCESSORS = (
+    'django.core.context_processors.csrf'
 )
 
 ROOT_URLCONF = 'Ring.urls'
