d0d00fcb7dc4009ddd510f61430132e62519ba51
Franklin <franklin.z.yang@gmail.com>
2012-08-02 00:19:09 -0700

    testing dashboard creation view_3

--- Ring/urls.py
+++ Ring/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^/?$','api.views.index'),
-    url(r'^dashboard/?$','api.views.dashboard');
+    url(r'^dashboard/?$','api.views.dashboard'),
     url(r'^create_user/?$','api.views.create_user'),
     url(r'^create_group/?$','api.views.create_group'),
     # Examples:
